037ce15b329bd82163c702f3137527d0976545cb
longfellow <longfellow.wang@gmail.com>
2024-11-10 19:18:13 +0800

    首页修改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -112,14 +112,6 @@ onMounted(() => {
               </svg>
             </a>
           </div>
-          <div class="col-auto d-none d-lg-block background-sky">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img"
-              aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-              <title>Placeholder</title>
-              <rect width="199%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
-                dy=".3em">Thumbnail</text>
-            </svg>
-          </div>
         </div>
       </div>
       <div class="col-md-4">
@@ -136,14 +128,6 @@ onMounted(() => {
               </svg>
             </a>
           </div>
-          <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img"
-              aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
-                dy=".3em">Thumbnail</text>
-            </svg>
-          </div>
         </div>
       </div>
       <div class="col-md-4">
@@ -159,14 +143,6 @@ onMounted(() => {
                 <use xlink:href="#chevron-right"></use>
               </svg>
             </a>
-          </div>
-          <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img"
-              aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
-                dy=".3em">Thumbnail</text>
-            </svg>
           </div>
         </div>
       </div>
--- src/views/UserHomeView.vue
+++ src/views/UserHomeView.vue
@@ -48,8 +48,7 @@ const activeTabTitle = computed(() => {
     }
 })
 
-const navItems = ['概览', '个人信息', '个人创作', '我的消息'];
-const sidebarItems = ['基本信息', '个人收藏', '我的签署', '收件地址'];
+const sidebarItems = ['基本信息', '个人创作', '个人收藏', '我的消息'];
 const activeNav = ref('个人信息');
 const activeSidebar = ref('基本信息');
 </script>
@@ -60,67 +59,51 @@ const activeSidebar = ref('基本信息');
 
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <div>
-                    <!-- Top Navigation -->
-                    <nav class="navbar navbar-expand-lg">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#">个人中心</a>
-                            <div class="navbar-nav">
-                                <a class="nav-link list-group-item list-group-item-action border-0 rounded"
+                <div class="container-fluid mt-3">
+                    <div class="row">
+                        <!-- Sidebar -->
+                        <div class="col-md-2">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action border-0 rounded"
                                     v-for="item in sidebarItems" :key="item" :class="{ active: activeSidebar === item }"
                                     @click="activeSidebar = item">
                                     {{ item }}
                                 </a>
                             </div>
                         </div>
-                    </nav>
 
-                    <div class="container-fluid mt-3">
-                        <div class="row">
-                            <!-- Sidebar -->
-                            <div class="col-md-2">
-                                <div class="list-group">
-                                    <a href="#" class="list-group-item list-group-item-action border-0 rounded"
-                                        v-for="item in sidebarItems" :key="item"
-                                        :class="{ active: activeSidebar === item }" @click="activeSidebar = item">
-                                        {{ item }}
-                                    </a>
-                                </div>
-                            </div>
-
-                            <!-- Main Content -->
-                            <div class="col-md-10">
-                                <div class="card border-0">
-                                    <div class="d-flex align-items-center mb-4">
-                                        <img src="https://github.com/mdo.png" alt="User Avatar"
-                                            class="rounded-circle me-3" style="width: 100px; height: 100px;">
-                                        <div>
-                                            <h3>longfellow</h3>
-                                            <p class="text-muted">暂未维护个人简介内容。</p>
-                                        </div>
+                        <!-- Main Content -->
+                        <div class="col-md-10">
+                            <div class="card border-0">
+                                <div class="d-flex align-items-center mb-4">
+                                    <img src="https://github.com/mdo.png" alt="User Avatar" class="rounded-circle me-3"
+                                        style="width: 100px; height: 100px;">
+                                    <div>
+                                        <h3>longfellow</h3>
+                                        <p class="text-muted">暂未维护个人简介内容。</p>
                                     </div>
+                                </div>
 
-                                    <!-- Basic Info -->
-                                    <h3>基本信息</h3>
-                                    <div class="row">
-                                        <div class="col-md-6 mb-3">
-                                            <strong>开发者类型：</strong> 岗位
-                                        </div>
-                                        <div class="col-md-6 mb-3">
-                                            <strong>应用开发者：</strong> 学生
-                                        </div>
-                                        <div class="col-md-6 mb-3">
-                                            <strong>行业：</strong>
-                                        </div>
-                                        <div class="col-md-6 mb-3">
-                                            <strong>社交通讯：</strong>
-                                        </div>
-                                        <div class="col-md-6 mb-3">
-                                            <strong>兴趣方向：</strong>
-                                        </div>
-                                        <div class="col-md-6 mb-3">
-                                            <strong>广告投放和流量变现：</strong>
-                                        </div>
+                                <!-- Basic Info -->
+                                <h3>基本信息</h3>
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <strong>开发者类型：</strong> 岗位
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <strong>应用开发者：</strong> 学生
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <strong>行业：</strong>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <strong>社交通讯：</strong>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <strong>兴趣方向：</strong>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <strong>广告投放和流量变现：</strong>
                                     </div>
                                 </div>
                             </div>
